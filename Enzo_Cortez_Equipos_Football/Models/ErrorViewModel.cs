3b030c9c44d2ebffe17aad6aa8fe5728d68d8d06
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-10-23 15:58:28 -0500

    Agregar archivos de proyecto.

---
+++ Enzo_Cortez_Equipos_Football/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Enzo_Cortez_Equipos_Football.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
